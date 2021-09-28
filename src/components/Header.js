7fbf79d3fbe8ba6d1cbbad65ef3482231264fa29
Dursun Koc <dursunKoc@gmail.com>
2021-09-28 18:56:34 +0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,41 @@
 import PropTypes from 'prop-types'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
+import { FaArrowRight } from 'react-icons/fa'
 import { Context } from '../Store'
 import Button from './Button'
 
 
 const Header = ({ title, onShowAddForm, onRefresh }) => {
     const [state, setState] = useContext(Context)
+    const [url, setUrl] = useState(state.kafkaConnectUrl)
 
     const onAdd = (e) => {
         onShowAddForm(e)
     }
 
+    const urlChange = (e) => {
+        setUrl(e.target.value)
+    }
+
+    const submitUrlUpdate = async (e)=>{
+        e.preventDefault()
+        setState({ ...state, kafkaConnectUrl: url })
+    }
+    const urlModified = url !== state.kafkaConnectUrl
     return (
         <header>
             <h1>{title}</h1>
-            <input value={state.kafkaConnectUrl} onChange={(e) => setState({ ...state, kafkaConnectUrl: e.target.value })} /><br/>
-            <Button color={'green'} text={'Add Connector'} onClick={onAdd} />
-            <Button color={'green'} text={'Refresh'} onClick={onRefresh} />
+            <form  className="webflow-style-input" onSubmit={submitUrlUpdate}>
+                <input value={url} onChange={urlChange} autoFocus/>
+                <button type="submit"><FaArrowRight/></button>
+            </form>
+            {urlModified&&<div className="container warn">Current Url: {state.kafkaConnectUrl}, submit to update</div>}
+            <Button color={urlModified?'gray':'green'} disabled={urlModified} text={'Add Connector'} onClick={onAdd} />
+            <Button color={urlModified?'gray':'green'} disabled={urlModified} text={'Refresh'} onClick={onRefresh}/>
+            
         </header>
     )
 }
-
-// const headingStyle = {
-//     color: 'green', backgroundColor: 'black',
-// }
-
 Header.defaultProps = {
     title: "Visual Connector",
     showAddForm: false,
